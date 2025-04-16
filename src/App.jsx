8566c082648555aaba9b7104c40bcbe5ae915272
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-16 14:04:16 +0200

    Added Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -105,10 +105,74 @@ async function fetchData(baseUrl) {
 
 // MILESTONE 3
 
+// function App() {
+// 
+//   const [politiciansList, setPoliticiansList] = useState([]); // Variabile di stato per memorizzare i dati della fetch.
+//   const [value, setValue] = useState("") // Variabile di stato con valore dell'input
+// 
+//   useEffect(() => {
+//     fetchData(baseUrl)
+//       .then(res => setPoliticiansList(res))
+//       .catch(error => console.error(error.stack))
+//   }, []); // Setta i dati al mounting del componenete.
+// 
+//   const filteredData = useMemo(() => {
+//     const filtredName = politiciansList.filter((element) => element.name.toLowerCase().includes(value.toLowerCase()) || element.biography.toLowerCase().includes(value.toLowerCase()))
+//     return filtredName
+//   }, [politiciansList, value])
+// 
+//   return (
+//     <>
+//       <div className="container">
+//         <input
+//           style={{ margin: "10px", borderRadius: "10px", border: "1px solid black", padding: "5px 10px" }}
+//           type="text"
+//           placeholder="Ricerca avanzata"
+//           onChange={(e) => { setValue(e.target.value) }}
+//           value={value}
+//         />
+//         <div className="row g-2 ">
+//           {filteredData.map((element) => {
+//             return <PoliticiansCard id={element.id} image={element.image} name={element.name} biography={element.biography} position={element.position} />
+//           })}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+// 
+// const PoliticiansCard = memo((props) => {
+// 
+//   console.log("Rendered:", props.id)
+// 
+//   return (
+//     <>
+//       <div key={props.id} className="card col-12 col-sm-6 col-md-4 col-lg-3 p-2">
+//         <img src={props.image} style={{ height: "300px" }} className="card-img-top container-fluid" alt="..." />
+//         <div className="card-body">
+//           <div style={{ height: "280px" }}>
+//             <h5 className="card-title">{name}</h5>
+//             <p className="card-text">{props.biography}</p>
+//             <p className="card-text"><strong>Role:</strong> {props.position}</p>
+//           </div>
+//           <a href="#" className="btn btn-primary">Show Details</a>
+//         </div>
+//       </div>
+//     </>
+//   )
+// })
+// 
+// 
+// 
+// export default App
+
+// MILESTONE 4 ( BONUS )
+
 function App() {
 
   const [politiciansList, setPoliticiansList] = useState([]); // Variabile di stato per memorizzare i dati della fetch.
   const [value, setValue] = useState("") // Variabile di stato con valore dell'input
+  const [selectValue, setSelectValue] = useState("")
 
   useEffect(() => {
     fetchData(baseUrl)
@@ -116,10 +180,19 @@ function App() {
       .catch(error => console.error(error.stack))
   }, []); // Setta i dati al mounting del componenete.
 
+  const derivated = politiciansList.reduce((acc, curr) => {
+    if (!acc.some((element) => element === curr.position)) {
+      acc.push(curr.position)
+    }
+    return acc
+  }, [])
+  console.log(derivated)
+
   const filteredData = useMemo(() => {
-    const filtredName = politiciansList.filter((element) => element.name.toLowerCase().includes(value.toLowerCase()) || element.biography.toLowerCase().includes(value.toLowerCase()))
+    const filtredName = politiciansList.filter((element) => element.name.toLowerCase().includes(value.toLowerCase()) || element.biography.toLowerCase().includes(value.toLowerCase()) || element.position.includes(selectValue))
     return filtredName
-  }, [politiciansList, value])
+  }, [politiciansList, value, selectValue])
+
 
   return (
     <>
@@ -131,6 +204,9 @@ function App() {
           onChange={(e) => { setValue(e.target.value) }}
           value={value}
         />
+        <select name="position" id="position" onChange={(e) => { setSelectValue(e.target.value) }}>
+          {derivated.map((element) => <option key={element.id} value={element}>{element}</option>)}
+        </select>
         <div className="row g-2 ">
           {filteredData.map((element) => {
             return <PoliticiansCard id={element.id} image={element.image} name={element.name} biography={element.biography} position={element.position} />
@@ -151,7 +227,7 @@ const PoliticiansCard = memo((props) => {
         <img src={props.image} style={{ height: "300px" }} className="card-img-top container-fluid" alt="..." />
         <div className="card-body">
           <div style={{ height: "280px" }}>
-            <h5 className="card-title">{name}</h5>
+            <h5 className="card-title">{props.name}</h5>
             <p className="card-text">{props.biography}</p>
             <p className="card-text"><strong>Role:</strong> {props.position}</p>
           </div>
